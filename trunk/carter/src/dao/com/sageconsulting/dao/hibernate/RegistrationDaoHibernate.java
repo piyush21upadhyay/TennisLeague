f6d6c8db1b24639c8e66ecdc7ef54dc5cf03224b
Piyush Upadhyay <piyush21upadhyay@gmail.com>
2018-09-22 16:47:43 +0530

    Registration Date logic
    Registration Date logic

--- trunk/carter/src/dao/com/sageconsulting/dao/hibernate/RegistrationDaoHibernate.java
+++ trunk/carter/src/dao/com/sageconsulting/dao/hibernate/RegistrationDaoHibernate.java
@@ -9,6 +9,7 @@
 package com.sageconsulting.dao.hibernate;
 
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import com.sageconsulting.dao.RegistrationDao;
@@ -32,22 +33,30 @@ public class RegistrationDaoHibernate extends BaseDaoHibernate implements Regist
     public List<Registration> getOpenRegistrations()
     {
         Date d = new Date();
-        return getHibernateTemplate().find("from Registration r where r.registrationEnd >= ? order by r.registrationEnd", d); //$NON-NLS-1$
+        //return getHibernateTemplate().find("from Registration r where r.registrationEnd >= ? order by r.registrationEnd", d); //$NON-NLS-1$
+        return fetchFinalValidList(getHibernateTemplate().find("from Registration r order by r.registrationEnd"), false, true, false);
     }
     
     @SuppressWarnings("unchecked")
     public List<Registration> getOpenRegistrationsForCity(Long cityId)
     {
         Date d = new Date();
+        /*GregorianCalendar gcalendar = new GregorianCalendar();
+        Date time=gcalendar.getTime();
         return getHibernateTemplate().find("from Registration r where r.registrationEnd >= ? and r.city.id = ? order by r.registrationEnd", //$NON-NLS-1$
-        		new Object[] { d, cityId });
+        		new Object[] { time, cityId });*/
+        List<Registration> registrationsList=getHibernateTemplate().find("from Registration r where r.city.id = ? order by r.registrationEnd", //$NON-NLS-1$
+        		new Object[] { cityId });
+        return fetchFinalValidList(registrationsList, false, true, false);
+        
     }
     
     @SuppressWarnings("unchecked")
     public List<Registration> getClosedRegistrations()
     {
         Date d = new Date();
-        return getHibernateTemplate().find("from Registration r where r.registrationEnd < ? order by r.registrationEnd", d); //$NON-NLS-1$
+        //return getHibernateTemplate().find("from Registration r where r.registrationEnd < ? order by r.registrationEnd", d); //$NON-NLS-1$
+        return fetchFinalValidList(getHibernateTemplate().find("from Registration r order by r.registrationEnd"), false, false, true);
     }
 
     public void saveRegistration(Registration registration)
@@ -60,5 +69,37 @@ public class RegistrationDaoHibernate extends BaseDaoHibernate implements Regist
     public void removeRegistration(Long id)
     {
         getHibernateTemplate().delete(getRegistration(id));
+    }
+    
+    /**
+     * @param registrationsList
+     * @param lessThanEqualTo
+     * @param grtrThanEqualTo
+     * @param lessThan
+     * @return
+     */
+    private List<Registration> fetchFinalValidList(List<Registration> registrationsList, boolean lessThanEqualTo, boolean grtrThanEqualTo, boolean lessThan){
+    	Iterator<Registration> itr=registrationsList.iterator();
+        while (itr.hasNext()) {
+			Registration registration = (Registration) itr.next();
+			Date todaysDate = new Date();
+			if(grtrThanEqualTo){
+				if(registration.getRegistrationEnd().before(todaysDate)){ 
+					itr.remove();
+				}
+			}else if(lessThanEqualTo){
+				if(registration.getRegistrationEnd().after(todaysDate)){ 
+					itr.remove();
+				}
+			}else {
+				if(lessThan){
+					continue;
+				}else{
+					itr.remove();
+				}
+			}
+				
+		}
+        return registrationsList;
     }
 }
